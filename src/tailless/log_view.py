0a6326207c95b688df53b87b940aff93c41cb962
Will McGugan <willmcgugan@gmail.com>
2024-01-09 16:49:47 +0000

    ui

--- src/tailless/log_view.py
+++ src/tailless/log_view.py
@@ -2,16 +2,24 @@ from itertools import islice
 import mmap
 import os
 import re
+from typing import NamedTuple, IO, TypeAlias
 
 from rich.text import Text
 from rich.highlighter import ReprHighlighter
 
+from textual import on
+from textual.app import ComposeResult
+from textual.containers import Vertical
 from textual.geometry import Size
 from textual.scroll_view import ScrollView
 from textual.cache import LRUCache
 from textual.strip import Strip
+from textual.widget import Widget
+from textual.widgets import Label
+from textual.reactive import reactive
 
-from typing import NamedTuple, IO, TypeAlias
+from .filter_dialog import FilterDialog
+
 
 OffsetPair: TypeAlias = tuple[int, int]
 
@@ -74,10 +82,10 @@ class MappedFile:
         except KeyError:
             line = self.get_line(line_index).rstrip("\n")
             text = Text(line)
+            text.expand_tabs(4)
             ReprHighlighter().highlight(text)
             self._text_cache[line_index] = text
-        text.expand_tabs(4)
-        return text
+        return text.copy()
 
     def _scan_line_breaks(self, start: int, end: int) -> list[int]:
         assert self._mmap is not None
@@ -99,18 +107,37 @@ class MappedFile:
         self._line_offsets[:] = offsets
 
 
-class LogView(ScrollView):
+class LogLines(ScrollView):
     DEFAULT_CSS = """
-    LogView {
-    
+    LogLines {
+        border: heavy transparent;
+        .loglines--filter-highlight {
+            background: $secondary;
+            color: auto;
+        }
+        &:focus {
+            border: heavy $accent;
+        }
     }
     """
+    COMPONENT_CLASSES = {"loglines--filter-highlight"}
+
+    show_find = reactive(False)
+    find = reactive("")
+    case_sensitive = reactive(False)
+    regex = reactive(False)
 
     def __init__(self, file_path: str) -> None:
         super().__init__()
         self.mapped_file = MappedFile(file_path)
         self._render_line_cache: LRUCache[int, Strip] = LRUCache(maxsize=1000)
         self._max_width = 0
+
+    def clear_caches(self) -> None:
+        self._render_line_cache.clear()
+
+    def notify_style_update(self) -> None:
+        self.clear_caches()
 
     def on_mount(self) -> None:
         self.mapped_file.open()
@@ -131,7 +158,10 @@ class LogView(ScrollView):
         except KeyError:
             text = self.mapped_file.get_text(index)
             text.stylize_before(style)
-            strip = Strip(text.render(self.app.console))
+
+            if self.find and self.show_find:
+                self.highlight_find(text)
+            strip = Strip(text.render(self.app.console), text.cell_len)
             self._render_line_cache[index] = strip
             self._max_width = max(self._max_width, strip.cell_length)
 
@@ -139,8 +169,92 @@ class LogView(ScrollView):
 
         return strip
 
+    def highlight_find(self, text: Text) -> None:
+        filter_style = self.get_component_rich_style("loglines--filter-highlight")
+        if self.regex:
+            try:
+                re.compile(self.find)
+            except Exception:
+                return
+            matches = list(
+                re.finditer(
+                    self.find,
+                    text.plain,
+                    flags=0 if self.case_sensitive else re.IGNORECASE,
+                )
+            )
+            if matches:
+                for match in matches:
+                    text.stylize(filter_style, *match.span())
+            else:
+                text.stylize("dim")
+        else:
+            if not text.highlight_words(
+                [self.find], filter_style, case_sensitive=self.case_sensitive
+            ):
+                text.stylize("dim")
+
     def on_idle(self) -> None:
         self.virtual_size = Size(self._max_width, self.mapped_file.line_count)
+
+    def watch_show_find(self) -> None:
+        self.clear_caches()
+
+    def watch_find(self) -> None:
+        self.clear_caches()
+
+    def watch_case_sensitive(self) -> None:
+        self.clear_caches()
+
+    def watch_regex(self) -> None:
+        self.clear_caches()
+
+
+class LogView(Vertical):
+    DEFAULT_CSS = """
+    LogView {
+        layers: dialogs;
+        .float {
+            layer: dialog;
+            offset-x: 120%;
+            transition: offset 200ms;
+            &.visible {
+                offset-x: 0%;
+            }
+        }
+    }
+    """
+
+    show_find = reactive(False)
+
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield LogLines(self.file_path)
+        yield FilterDialog()
+
+    @on(FilterDialog.Update)
+    def filter_dialog_update(self, event: FilterDialog.Update) -> None:
+        log_lines = self.query_one(LogLines)
+        log_lines.find = event.find
+        log_lines.regex = event.regex
+        log_lines.case_sensitive = event.case_sensitive
+
+    def watch_show_find(self, show_find: bool) -> None:
+        filter_dialog = self.query_one(FilterDialog)
+        filter_dialog.set_class(show_find, "visible")
+        self.query_one(LogLines).show_find = show_find
+        if show_find:
+            filter_dialog.query_one("Input").focus()
+        else:
+            self.query_one(LogLines).focus()
+
+    @on(FilterDialog.Dismiss)
+    def dismiss_filter_dialog(self, event: FilterDialog.Dismiss) -> None:
+        event.stop()
+        self.show_find = False
 
 
 if __name__ == "__main__":
--- src/tailless/ui.py
+++ src/tailless/ui.py
@@ -1,17 +1,48 @@
+from __future__ import annotations
+
 from textual.app import App, ComposeResult
+from textual.screen import Screen
 from textual.widgets import Footer, TabbedContent, TabPane
 
 from .log_view import LogView
 
 
+class LogScreen(Screen):
+    CSS = """
+    LogScreen {
+        TabPane {
+            padding: 0;
+        }
+    }
+    """
+
+    BINDINGS = [("ctrl+f", "toggle_find", "Find")]
+
+    def compose(self) -> ComposeResult:
+        assert isinstance(self.app, UI)
+        with TabbedContent():
+            for path in self.app.file_paths:
+                with TabPane(path):
+                    yield LogView(path)
+        yield Footer()
+
+    def action_toggle_find(self) -> None:
+        tabbed_content = self.query_one(TabbedContent)
+        pane = tabbed_content.get_pane(tabbed_content.active)
+        log_view = pane.query_one(LogView)
+        log_view.show_find = not log_view.show_find
+
+
 class UI(App):
+    CSS = """
+    Screen {
+
+    }    
+    """
+
     def __init__(self, file_paths: list[str]) -> None:
         self.file_paths = file_paths
         super().__init__()
 
-    def compose(self) -> ComposeResult:
-        with TabbedContent():
-            for path in self.file_paths:
-                with TabPane(path):
-                    yield LogView(path)
-        yield Footer()
+    def on_mount(self) -> None:
+        self.push_screen(LogScreen())
