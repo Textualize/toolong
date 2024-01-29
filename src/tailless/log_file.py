fac75169dd8932295249606dc40d26a6830082ec
Will McGugan <willmcgugan@gmail.com>
2024-01-29 10:59:03 +0000

    bzip support

--- src/tailless/log_file.py
+++ src/tailless/log_file.py
@@ -1,7 +1,12 @@
 import os
+import mimetypes
 from pathlib import Path
 from typing import IO
 from threading import Event
+
+
+class LogError(Exception):
+    pass
 
 
 class LogFile:
@@ -21,33 +26,47 @@ class LogFile:
 
     def open(self, exit_event: Event) -> bool:
         ext = self.path.suffix.lower()
-        if ext == ".gz":
-            return self.open_gzip(exit_event)
+
+        _, encoding = mimetypes.guess_type(self.path.name, strict=False)
+
+        if encoding in ("gzip", "bzip2"):
+            return self.open_compressed(exit_event, encoding)
 
         self.file = open(self.path, "rb", buffering=0)
         self.file.seek(0, os.SEEK_END)
         self.size = self.file.tell()
         self.can_tail = True
-        # self.fileno = os.dup(fileno)
-        # self.file = os.fdopen(self.fileno, "rb", buffering=1024 * 64)
-        # self.size = size
 
         return True
 
-    def open_gzip(self, exit_event: Event) -> bool:
-        import gzip
+    def open_compressed(self, exit_event: Event, encoding: str) -> bool:
         from tempfile import TemporaryFile
 
         chunk_size = 1024 * 256
 
         temp_file = TemporaryFile("wb+")
 
-        with gzip.open(self.path, "rb") as gzipped_file:
-            while data := gzipped_file.read(chunk_size):
+        compressed_file: IO[bytes]
+        if encoding == "gzip":
+            import gzip
+
+            compressed_file = gzip.open(self.path, "rb")
+        elif encoding == "bzip":
+            import bz2
+
+            compressed_file = bz2.open(self.path, "rb")
+        else:
+            # Shouldn't get here
+            raise AssertionError("Not supported")
+
+        try:
+            while data := compressed_file.read(chunk_size):
                 temp_file.write(data)
                 if exit_event.is_set():
                     temp_file.close()
                     return False
+        finally:
+            compressed_file.close()
 
         self.file = temp_file
         self.size = temp_file.tell()
